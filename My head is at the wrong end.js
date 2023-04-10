108497fd09dfb0efba772e4a6af7f87c2a5b7cd6
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2023-04-10 20:37:29 +0300

    My head is at the wrong end!

---
+++ My head is at the wrong end.js
@@ -0,0 +1,9 @@
+
+function fixTheMeerkat(arr) {
+  return arr.reverse(); 
+  // return [arr.pop(), arr[1], arr.shift()];
+}
+
+// function fixTheMeerkat([tail, body, head]) {
+//  return [head, body, tail];
+// }
